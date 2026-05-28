53545993ba9f6ac2af99da6fcc7cd11c31107fd6
Taha Aflouk <tahaaflouk94@gmail.com>
2026-05-28 19:58:36 +0100

    SCRUM-19 configure monitoring service database integration

---
+++ src/main/java/com/edgecloud/monitoring/EdgecloudMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edgecloud.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgecloudMonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgecloudMonitoringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edgecloud/monitoring/EdgecloudMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgecloud.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgecloudMonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
