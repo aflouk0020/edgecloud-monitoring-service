b9322b6a937a45339fcf0ed9a494cb87b913a971
Taha Aflouk <tahaaflouk94@gmail.com>
2026-06-20 12:29:20 +0100

    SCRUM-74 add monitored services list endpoint

--- src/main/java/com/edgecloud/monitoring/service/MonitoredServiceService.java
+++ src/main/java/com/edgecloud/monitoring/service/MonitoredServiceService.java
@@ -3,9 +3,12 @@ package com.edgecloud.monitoring.service;
 import com.edgecloud.monitoring.dto.MonitoredServiceRegistrationRequest;
 import com.edgecloud.monitoring.dto.MonitoredServiceResponse;
 
+import java.util.List;
+
 public interface MonitoredServiceService {
 
     MonitoredServiceResponse registerService(
             MonitoredServiceRegistrationRequest request);
 
+    List<MonitoredServiceResponse> getAllServices();
 }
